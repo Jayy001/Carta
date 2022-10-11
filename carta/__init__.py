307d47294826a5dcdd6b5d3f6abab5972226b786
unknown <jayy2004@gmx.co.uk>
2022-10-11 19:22:45 +0100

    Added relative . to imports

--- carta/__init__.py
+++ carta/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.1"
 
-from __main__ import ReMarkable, Widget
+from .__main__ import ReMarkable, Widget
 
 if __name__ == "__main__":
-    print("You cannot run this libary directly")  # pragma: no cover
+    print("You cannot run this library directly")  # pragma: no cover
